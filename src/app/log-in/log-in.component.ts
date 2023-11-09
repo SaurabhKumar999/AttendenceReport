d9dd551b9203d4e22e237cdaf7178f245b7e29df
SaurabhKumar999 <saurabhjoenish@gmail.com>
2023-11-09 17:19:44 +0530

    Attendence Report

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,26 +1,40 @@
-import { Component } from '@angular/core';
-import{Router} from '@angular/router'
+import { Component, OnInit } from '@angular/core';
+import{Router} from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.css']
 })
-export class LogInComponent {
-  email: string = '';
-  password: string = '';
+export class LogInComponent implements OnInit{
+ public loginForm!: FormGroup;
+  formBuilder: any;
+
    
-  constructor(private router: Router) {}
- 
-  onSubmit() {
- 
-    console.log('Email:', this.email);
-    console.log('Password:', this.password);
+  constructor(private router: Router ,private fb:FormBuilder, private http :HttpClient) {}
+  ngOnInit(): void {
+   this.loginForm=this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+   })
   }
-onSignupClick(){
-  this.router.navigate(['/sign-up']);
-}
-onDashboardClick(){
+  logIn()
+  {
+this.http.get<any>("http://localhost:4200/").subscribe(res=>{
+  const user= res.find((a:any)=>{
+    return a.email === this.loginForm.value.email && this.loginForm.value.password
+  });
+  if(user){
+    alert("Login Successfully");
+    this.loginForm.reset();
+    this.router.navigate(['/Dashboard'])
+  }else{
+    alert("User not found");
+  }
+  },err=>{
+    alert("Somthing went wrong");
+  })
  
-  this.router.navigate(['/Dashboard']);
-}
+  }
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -7,10 +8,11 @@ import { Router } from '@angular/router';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent {
-  signupForm: FormGroup;
+export class SignUpComponent implements OnInit{
+ public signupForm !: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router, private http : HttpClient) {  }
+  ngOnInit(): void {
     this.signupForm = this.fb.group({
       firstname: ['', Validators.required],
       lastname: ['', Validators.required],
@@ -18,20 +20,17 @@ export class SignUpComponent {
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmpassword: ['', Validators.required]
     }, { validator: this.passwordMatchValidator });
+    throw new Error('Method not implemented.');
   }
   signUp(): void {
     if (this.signupForm.valid) {
-      
-      console.log(this.signupForm.value);
-      
+      this.http.post<any>("http://localhost:4200/", this.signupForm.value);
+      // console.log(this.signupForm.value);
+      alert("Sign up Successfully");
+      this.signupForm.reset();
       this.router.navigate(['/log-in']);
     }
   }
-  onLoginClick(): void {
-    // Handle login button click
-    this.router.navigate(['/log-in']);
-  }
-
   passwordMatchValidator(form: FormGroup): null | object {
     const password = form.get('password');
     const confirmpassword = form.get('confirmpassword');
